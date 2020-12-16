f890e5dc196fa961fa05f16634fea40383b55aa8
st-em <75288937+st-em@users.noreply.github.com>
2020-12-16 16:07:40 +0900

    Update ProfileController.php

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -15,7 +15,7 @@ class ProfileController extends Controller
 
     public function create()
     {
-        return redirect('admin/profile/create');
+        return redirect('admin.profile.create');
     }
 
     public function edit()
@@ -25,7 +25,7 @@ class ProfileController extends Controller
 
     public function update()
     {
-        return redirect('admin/profile/edit');
+        return redirect('admin.profile.edit');
     }
     
     
